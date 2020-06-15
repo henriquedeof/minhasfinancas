18beb304a4662a1fef9ef0532c6b28bf02dc3a96
henriquedeof <henriquedeof@gmail.com>
2020-06-15 23:52:18 +1000

    Initial commit

---
+++ src/main/java/au/com/xpto/minhasfinancas/MinhasfinancasApplication.java
@@ -0,0 +1,13 @@
+package au.com.xpto.minhasfinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhasfinancasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinhasfinancasApplication.class, args);
+    }
+
+}
